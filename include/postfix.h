f329fe88607bfa2bc60ecbcf1da4309adcb6276f
tndumarevskaya <dumtanya2009@gmail.com>
2021-01-19 14:07:55 +0300

    fix

--- include/postfix.h
+++ include/postfix.h
@@ -1,10 +1,9 @@
 // Copyright 2020 Dumarevskaya
+
 #ifndef INCLUDE_POSTFIX_H_
 #define INCLUDE_POSTFIX_H_
-
 #include <string>
-#include "MyStack.h"
 
-std::string infix2prefix(std::string);
+std::string infix2postfix(std::string);
 
 #endif  // INCLUDE_POSTFIX_H_
